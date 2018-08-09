1ea88d4827910997fac59898c713faeebf879fa4
Daniel Anton Mata <daniel.mata@sykes.com>
2018-08-10 03:02:04 +0800

    Added usage of DevTools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import DevTools from 'mobx-react-devtools';
 
 import TodoView from './views/TodoView';
 
@@ -8,6 +9,11 @@ const store = new TodoStore();
 
 export default class App extends Component {
   render() {
-    return <TodoView store={store} />;
+    return (
+      <Fragment>
+        <TodoView store={store} />
+        <DevTools />
+      </Fragment>
+    );
   }
 }
